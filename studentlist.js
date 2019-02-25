52b2ef3490002a706326b2313c1401d2d2013e5d
Karoline Lüthje <Karoline@Karolines-MacBook-Pro-4.local>
2019-02-25 22:11:53 +0100

    new and improved version according to the submission description

---
+++ studentlist.js
@@ -0,0 +1,7 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
+}
